e9bce1330f739c390e3ef97a05886d3f125c63a7
evy <evyco3@gmail.com>
2024-04-12 13:23:53 +0300

    Intial commit

---
+++ src/test/java/org/example/tests/AppTest.java
@@ -0,0 +1,12 @@
+package org.example.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+    @Test
+    public void TC001(){
+        Assert.assertTrue(true);
+    }
+}
